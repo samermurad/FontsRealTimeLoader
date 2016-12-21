3605eed7c1db8091cd8cc0d7a6c3f7d45a615566
Samer Murad <samermurad555@gmail.com>
2016-12-21 19:16:58 +0200

    Added Project

---
+++ FontsRealTimeLoader/File.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  FontsRealTimeLoader
+//
+//  Created by Samer Murad on 21/12/2016.
+//  Copyright © 2016 Samer Murad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+public extension UIFont {
+    private static let domain = "com.samermurad.FontsRealTimeLoader"
+    public static func register(from url: URL) throws {
+        
+        guard let fontDataProvider = CGDataProvider(url: url as CFURL) else {
+            throw NSError(domain: domain, code: 123, userInfo: ["msg":"Could not create font data provider for \(url)."])
+        }
+        let font = CGFont(fontDataProvider)
+        var error: Unmanaged<CFError>?
+        guard CTFontManagerRegisterGraphicsFont(font, &error) else {
+            throw error!.takeUnretainedValue()
+        }
+    }
+    //        public static func fontsURLs() -> [URL] {
+    //            let bundle = Bundle.sdk
+    //            let fileNames = ["OpenSans-Light", "OpenSans-Regular", "OpenSans-Semibold"]
+    //            return fileNames.map({ bundle.url(forResource: $0, withExtension: "ttf")! })
+    //        }
+}
