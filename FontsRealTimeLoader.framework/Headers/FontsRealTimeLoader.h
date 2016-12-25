ea0d786b3a5ad26598f2f5a470ea708dbc9c2d2b
Samer Murad <samermurad555@gmail.com>
2016-12-25 15:57:23 +0200

    Changed Pod to dynamic framework

---
+++ FontsRealTimeLoader.framework/Headers/FontsRealTimeLoader.h
@@ -0,0 +1,19 @@
+//
+//  FontsRealTimeLoader.h
+//  FontsRealTimeLoader
+//
+//  Created by Samer Murad on 21/12/2016.
+//  Copyright © 2016 Samer Murad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontsRealTimeLoader.
+FOUNDATION_EXPORT double FontsRealTimeLoaderVersionNumber;
+
+//! Project version string for FontsRealTimeLoader.
+FOUNDATION_EXPORT const unsigned char FontsRealTimeLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontsRealTimeLoader/PublicHeader.h>
+
+
